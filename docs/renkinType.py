b44bd4966bd0e82701fe7004c15be3da17068689
yukihane <yukihane.feather@gmail.com>
2015-05-20 20:33:02 +0900

    JSONフォーマットは末尾のカンマが許容されないようなので修正

--- docs/renkinType.py
+++ docs/renkinType.py
@@ -11,17 +11,23 @@ filename = sys.argv[1]
 f = open(filename, "r")
 reader = csv.reader(f, delimiter = "\t")
 next(reader)
+isFirst = True
+
 print('{')
 for elm in reader:
   if elm[1]:
+    if isFirst:
+      isFirst = False
+    else:
+      print('  ,')
     print('  "' + elm[1] + '": {')
     print('    "id": ' + elm[1] + ',')
     print('    "name": "' + elm[0] + '",')
     print('    "scale": ' + elm[2] + ',')
     print('    "min": ' + elm[3] + ',')
     print('    "max": ' + elm[4] + ',')
-    print('    "tickInv": ' + str(int(1/float(elm[5]))) + ',')
-    print('  },')
+    print('    "tickInv": ' + str(int(1/float(elm[5]))) )
+    print('  }')
 print('}')
 
 f.close()
