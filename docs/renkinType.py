3b9700e14e628a834e53fdae74263669184fe09c
yukihane <yukihane.feather@gmail.com>
2015-05-20 16:11:53 +0900

    錬金の種類定義

---
+++ docs/renkinType.py
@@ -0,0 +1,26 @@
+#! /usr/bin/python3
+# -*- coding: utf-8 -*-
+
+# bazaar_searching_conditions.ods の "錬金効果" シートを tsv 形式で保存して
+# このスクリプトに読み込ませます
+
+import sys
+import csv
+
+filename = sys.argv[1]
+f = open(filename, "r")
+reader = csv.reader(f, delimiter = "\t")
+next(reader)
+print("{")
+for elm in reader:
+    print("  " + elm[1] + ": {")
+    print("    id: " + elm[1] + ",")
+    print("    name: '" + elm[0] + "',")
+    print("    scale: " + elm[2] +",")
+    print("    min: " + elm[3] + ",")
+    print("    max: " + elm[4] + ",")
+    print("    tickInv: " + str(int(1/float(elm[5]))) + ",")
+    print("  },")
+print("}")
+
+f.close()
