d7cb8644319e7653213b80cc95506eafed5030ea
yukihane <yukihane.feather@gmail.com>
2015-06-04 05:25:50 +0900

    モーモンバザー雛形

--- main.js
+++ main.js
@@ -518,6 +518,35 @@ function($scope, $http, $log, loginService) {
 
 }]);
 
+
+mainModule.controller("houseBazaarCtrl", ["$scope", "$http", "$log", "loginService",
+function($scope, $http, $log, loginService) {
+
+  $scope.reload = function() {
+
+    console.log(loginService.sessionId);
+
+    var req = {
+      method: "GET",
+      url: "https://happy.dqx.jp/capi/housing/bazaar/history/",
+      headers: {
+        "X-Smile-3DS-SESSIONID": loginService.character.sessionId,
+      },
+    };
+
+    $http(req)
+    .success(function(data, status, headers, config) {
+      console.log(data);
+      $scope.debug = data;
+    })
+    .error(function(data, status, headers, config) {
+    });
+
+  };
+
+}]);
+
+
 mainModule.controller("bazaarCtrl", ["$scope", "$http", "$resource", "$log", "loginService",
 function($scope, $http, $resource, $log, loginService) {
   
