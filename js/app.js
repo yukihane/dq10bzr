3c798fc2e951865386350d09bc256ac0b5cdc9b4
yukihane <yukihane.feather@gmail.com>
2015-06-08 15:33:07 +0900

    tobatsuCtrlファイル分割

--- js/app.js
+++ js/app.js
@@ -53,35 +53,6 @@ mainModule.filter("epochStrToJpDateStr", function(){
 
 
 
-mainModule.controller("tobatsuCtrl", ["$scope", "$http", "$log", "loginService", 
-function($scope, $http, $log, loginService) {
-
-  $scope.list = [];
-
-  $scope.reload = function() {
-  
-    console.log(loginService.sessionId);
-
-    var req = {
-      method: "GET",
-      url: "https://happy.dqx.jp/capi/tobatsu/tobatsulist/",
-      headers: {
-        "X-Smile-3DS-SESSIONID": loginService.character.sessionId,
-      },
-    };
-    
-
-    console.log("討伐リクエスト");
-    $http(req)
-    .success(function(data, status, headers, config) {
-      console.log(data);
-      $scope.list = data;
-    })
-    .error(function(data, status, headers, config) {
-    });
-
-  };
-}]);
 
 
 mainModule.controller("syokuninCtrl", ["$scope", "$http", "$log", "loginService",
