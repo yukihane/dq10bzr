a3e60018f04d8c0eda519b0ddbeb8784d682bae4
yukihane <yukihane.feather@gmail.com>
2015-07-02 21:12:00 +0900

    討伐リクエストをサービス化

--- js/controllers/tobatsuCtrl.js
+++ js/controllers/tobatsuCtrl.js
@@ -3,53 +3,19 @@
 /*
 討伐タブコントローラ.
 */
-angular.module("dq10bzr.Main").controller("tobatsuCtrl", ["$rootScope", "$scope", "$http", "$log", "loginService", 
-function($rootScope, $scope, $http, $log, loginService) {
+angular.module("dq10bzr.Main").controller("tobatsuCtrl", ["$rootScope", "$scope", "$log", "request",
+function($rootScope, $scope, $log, request) {
 
   $scope.list = [];
 
   $scope.reload = function() {
-  
-    console.log(loginService.sessionId);
 
-    var req = {
-      method: "GET",
-      url: "https://happy.dqx.jp/capi/tobatsu/tobatsulist/",
-      headers: {
-        "X-Smile-3DS-SESSIONID": loginService.character.sessionId,
-      },
-    };
-    
-
-    console.log("討伐リクエスト");
-    $http(req)
-    .success(function(data, status, headers, config) {
-      if(data.resultCode !== 0) {
-        var msg = getErrorMsg(data.resultCode);
-        $rootScope.$broadcast("footer.notify", msg);
-        return;
-      }
-
+    var promise = request.tobatsu();
+    promise.then(function(data) {
       $scope.list = data;
-
-    })
-    .error(function(data, status, headers, config) {
-      $rootScope.$broadcast("footer.notify", "通信エラー");
-      console.log(data);
+    }, function(msg) {
+      $rootScope.$broadcast("footer.notify", msg);
     });
-
-  };
-
-  var getErrorMsg = function(code) {
-
-    switch(code) {
-      case 0:
-        return "";
-      case 106:
-        return "ログイン中は本操作を実行できません";
-      default:
-        return "エラー発生(" + code + ")";
-    }
   };
 
 }]);
--- js/services/request.js
+++ js/services/request.js
@@ -12,12 +12,24 @@ angular.module("dq10bzr.Main").factory("request", ["$http", "$q", "loginService"
   };
 
   // HTTPレスポンスコードからメッセージを生成します
-  var getMessage = function(status) {
+  var getHttpMessage = function(status) {
     switch(status) {
       case 401:
         return "認証に失敗しました。再ログインが必要です。";
       default:
         return "要求が失敗しました";
+    }
+  };
+
+  var getErrorMsg = function(code) {
+
+    switch(code) {
+      case 0:
+        return "";
+      case 106:
+        return "ログイン中は本操作を実行できません";
+      default:
+        return "エラー発生(" + code + ")";
     }
   };
 
@@ -38,7 +50,7 @@ angular.module("dq10bzr.Main").factory("request", ["$http", "$q", "loginService"
       deferred.resolve(data);
     })
     .error(function(data, status, headers, config) {
-      var msg = getMessage(status);
+      var msg = getHttpMessage(status);
       deferred.reject(msg);
     });
 
@@ -46,7 +58,37 @@ angular.module("dq10bzr.Main").factory("request", ["$http", "$q", "loginService"
 
   };
 
+  var tobatsu = function() {
+
+    var req = {
+      method: "GET",
+      url: "https://happy.dqx.jp/capi/tobatsu/tobatsulist/",
+      headers: {
+        "X-Smile-3DS-SESSIONID": getSessionId(),
+      },
+    };
+
+    var deferred = $q.defer();
+
+    $http(req)
+    .success(function(data, status, headers, config) {
+      if(data.resultCode !== 0) {
+        var msg = getErrorMsg(data.resultCode);
+        deferred.reject(msg);
+      }
+
+      deferred.resolve(data);
+    })
+    .error(function(data, status, headers, config) {
+      var msg = getHttpMessage(status);
+      deferred.reject(msg);
+    });
+
+    return deferred.promise;
+  };
+
   return {
     friends: friends,
+    tobatsu: tobatsu,
   };
 }]);
