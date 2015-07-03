0aa9272f515d67b52381bf1436ba5b3cafc29c73
yukihane <yukihane.feather@gmail.com>
2015-07-03 12:06:24 +0900

    フォーマット

--- js/app.js
+++ js/app.js
@@ -4,12 +4,13 @@ var OAUTH_URL = "https://secure.square-enix.com/oauth/oa/";
 
 var mainModule = angular.module("dq10bzr.Main", ["ui.bootstrap", "ngResource"]);
 
-mainModule.config(["$logProvider", function($logProvider){
-  $logProvider.debugEnabled(true);
-}]);
+mainModule.config(["$logProvider", function ($logProvider) {
+    $logProvider.debugEnabled(true);
+  }
+]);
 
 // アカウント(現在は1アカウントのみを想定)情報を管理するサービス
-mainModule.factory("loginService", function(){
+mainModule.factory("loginService", function () {
   return {
     // 認証情報
     // この情報を持っていればキャラクタ選択可能
@@ -17,12 +18,10 @@ mainModule.factory("loginService", function(){
       cis_sessid: null,
       _c: null,
     },
-
     character: {
       // キャラクタ選択をすればこの情報が取得できる
       // X-Smile-3DS-SESSIONID ヘッダ情報として付与する
       sessionId: null,
-
       // キャラクタ情報
       // キャラクタ名
       characterName: null,
@@ -34,9 +33,9 @@ mainModule.factory("loginService", function(){
   };
 });
 
-mainModule.filter("convertToProgress", function() {
-  return function(isComplete) {
-    if(isComplete) {
+mainModule.filter("convertToProgress", function () {
+  return function (isComplete) {
+    if (isComplete) {
       return "済";
     }
     return "未";
@@ -45,11 +44,11 @@ mainModule.filter("convertToProgress", function() {
 
 
 /*
-エポックミリ秒文字列を受け取り、「日本時間帯における」月日文字列を返します.
-*/
-mainModule.filter("epochStrToJpDateStr", function(){
-  return function(epoch) {
-    var offsetEpoch = parseInt(epoch, 10) + 9*60*60*1000;
+ エポックミリ秒文字列を受け取り、「日本時間帯における」月日文字列を返します.
+ */
+mainModule.filter("epochStrToJpDateStr", function () {
+  return function (epoch) {
+    var offsetEpoch = parseInt(epoch, 10) + 9 * 60 * 60 * 1000;
     var date = new Date(offsetEpoch);
     return "" + (date.getUTCMonth() + 1) + "/" + date.getUTCDate();
   };
